2401f3ce538c531c92a007c51c35e9cd40cba7d7
Evan Ilukhin <evanilukhin@gmail.com>
2019-02-27 08:38:05 +0300

    Change prod config

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,8 @@ config :demiurge,
 
 config :demiurge, DemiurgeWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "localhost", port: System.get_env("PORT")],
+  http: [ip: {127,0,0,1}, port: {:system, "PORT"}],
+  url: [host: {:system, "HOST"}, port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
