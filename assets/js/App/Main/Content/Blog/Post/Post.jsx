389fa0749269a0b11689493d1603a35364ae5538
Evan Ilukhin <evanilukhin@gmail.com>
2019-05-05 23:09:09 +0300

    Style post

--- assets/js/App/Main/Content/Blog/Post/Post.jsx
+++ assets/js/App/Main/Content/Blog/Post/Post.jsx
@@ -6,9 +6,15 @@ import style from './Post.less'
 export default function Post({post}) {
   return(
     <div className={style.main}>
+      <div className={style.date}>
+        <span>
+          05-05-2019
+        </span>
+      </div>
       <div className={style.header}>
         {post.header}
       </div>
+      <div className={style.line}/>
       <Markup className={style.preview} content={post.mainPart} />
     </div>
   );
