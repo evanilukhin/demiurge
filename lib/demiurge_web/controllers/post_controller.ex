9c85a4f2a145167d517032a87fcfa09064f10f89
Evan Ilukhin <evanilukhin@gmail.com>
2019-05-20 08:28:33 +0300

    Add meta in server response

--- lib/demiurge_web/controllers/post_controller.ex
+++ lib/demiurge_web/controllers/post_controller.ex
@@ -4,12 +4,19 @@ defmodule DemiurgeWeb.PostController do
   def show(conn, params) do
     refined_params = params |> Map.new(fn {k, v} -> {String.to_atom(k), v} end)
     {:ok, post} = Demiurge.Post.Helpers.get_post(refined_params)
-    %Demiurge.Post{summary: summary, header: header, head_image: _head_image} = post
-    attrs_list = [%{name: "og:description", content: summary},
-                  %{name: "og:title", content: header},
-                  %{property: "og:url", content: "/posts/" <> params[:id]},
-                  %{property: "og:image", content: "https://cdn.pixabay.com/photo/2019/04/28/18/34/purple-4163951_960_720.jpg"},
-                ]
-    render(conn, "index.html", meta_attrs: attrs_list)
+    render(conn, "index.html", meta_attrs: post_attributes(post, conn, params))
+  end
+
+  defp post_attributes(post, conn, params) do
+    %Demiurge.Post{summary: summary, header: header, head_image: head_image} = post
+    [
+      %{name: "og:description", content: summary},
+      %{name: "description", content: summary},
+      %{name: "author", content: "Ivan Ilukhin"},
+      %{name: "title", content: header <> " | Ivan Ilukhin"},
+      %{name: "og:title", content: header},
+      %{property: "og:url", content: DemiurgeWeb.Router.Helpers.url(conn) <> "/posts/" <> params["id"]},
+      %{property: "og:image", content: head_image},
+    ]
   end
 end
