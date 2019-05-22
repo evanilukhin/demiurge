787e8dfb215611debb722f68a9b36c32a7ead32d
Evan Ilukhin <evanilukhin@gmail.com>
2019-05-22 08:45:25 +0300

    Add library and all necessary butons

--- assets/js/App/Main/Content/Blog/Index/PostCard/PostCard.jsx
+++ assets/js/App/Main/Content/Blog/Index/PostCard/PostCard.jsx
@@ -4,6 +4,18 @@ import ReactMarkdown from 'react-markdown';
 import SharePanel from '../../Post/SharePanel'
 import style from './PostCard.less'
 
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  RedditShareButton,
+
+  FacebookIcon
+} from 'react-share';
+
 export default function PostCard({post}) {
   let showMoreLink;
   if (!post.short){
--- assets/js/App/Main/Content/Blog/Post/SharePanel.jsx
+++ assets/js/App/Main/Content/Blog/Post/SharePanel.jsx
@@ -1,5 +1,14 @@
 import React from "react"
-import { Twitter } from 'react-social-sharing'
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  VKShareButton,
+  RedditShareButton,
+
+  FacebookIcon
+} from 'react-share';
 
 import style from './SharePanel.less'
 
@@ -7,10 +16,47 @@ import style from './SharePanel.less'
 export default function SharePanel({post}) {
   return(
     <div className={style.main}>
-      <Twitter
-        link={window.location.origin + '/posts/' + post.id}
-        label={post.header}
-      />
+      <FacebookShareButton
+        url={window.location.origin + "/posts/" + post.id}
+        quote={post.summary}
+        className="Demo__some-network__share-button">
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+
+      <LinkedinShareButton
+        url={window.location.origin + "/posts/" + post.id}
+        quote={post.summary}
+        className="Demo__some-network__share-button">
+        <FacebookIcon size={32} round />
+      </LinkedinShareButton>
+
+      <TwitterShareButton
+        url={window.location.origin + "/posts/" + post.id}
+        quote={post.summary}
+        className="Demo__some-network__share-button">
+        <FacebookIcon size={32} round />
+      </TwitterShareButton>
+
+      <TelegramShareButton
+        url={window.location.origin + "/posts/" + post.id}
+        quote={post.summary}
+        className="Demo__some-network__share-button">
+        <FacebookIcon size={32} round />
+      </TelegramShareButton>
+
+      <VKShareButton
+        url={window.location.origin + "/posts/" + post.id}
+        quote={post.summary}
+        className="Demo__some-network__share-button">
+        <FacebookIcon size={32} round />
+      </VKShareButton>
+
+      <RedditShareButton
+        url={window.location.origin + "/posts/" + post.id}
+        quote={post.summary}
+        className="Demo__some-network__share-button">
+        <FacebookIcon size={32} round />
+      </RedditShareButton>
     </div>
   );
 }
