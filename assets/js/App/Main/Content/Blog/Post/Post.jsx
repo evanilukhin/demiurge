f428dba096e8358bf58223e1ff984234814b5d1b
Evan Ilukhin <evanilukhin@gmail.com>
2019-07-30 09:34:17 +0300

    Refactor post

--- assets/js/App/Main/Content/Blog/Post/Post.jsx
+++ assets/js/App/Main/Content/Blog/Post/Post.jsx
@@ -1,24 +1,21 @@
 import React from "react"
 import ReactMarkdown from 'react-markdown';
 import SharePanel from './SharePanel'
-
 import style from './Post.less'
 import Code from "./Code";
 
 export default function Post({post}) {
-  return(
-    <div className={style.main}>
-      <div className={style.date}>
-        <span>
-          {post.insertedAt}
-        </span>
-      </div>
-      <div className={style.header}>
-        {post.header}
-      </div>
-      <div className={style.line}/>
-      <ReactMarkdown source={post.mainPart} escapeHtml={false} renderers={{ code: Code }}/>
-      <SharePanel post={post}/>
-    </div>
-  );
+    return (
+        <div className={style.main}>
+            <div className={style.date}>
+            <span> {post.insertedAt}</span>
+            </div>
+            <div className={style.header}>
+                {post.header}
+            </div>
+            <div className={style.line}/>
+            <ReactMarkdown source={post.mainPart} escapeHtml={false} renderers={{code: Code}}/>
+            <SharePanel post={post}/>
+        </div>
+    );
 }
