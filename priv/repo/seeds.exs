8b36c0a713a119034544f82a961f99af8139385e
evanilukhin <evanilukhin@gmail.com>
2020-10-23 09:35:35 +0300

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Seed posts
+
+File.read!("priv/data/en/posts_seed.json")
+|> Jason.decode!()
+|> Enum.each(fn post -> Demiurge.Post.changeset(%Demiurge.Post{:state => "published", :short => false}, post) |> Demiurge.Repo.insert! end)
