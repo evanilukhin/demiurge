a9d778feddd1b4fbdea0731f77570b77548b3682
evanilukhin <evanilukhin@gmail.com>
2021-02-08 01:22:02 +0300

    Update CORS for production

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,26 @@ config :demiurge, DemiurgeWeb.Endpoint, server: true
 config :demiurge, DemiurgeWeb.Endpoint,
   secret_key_base: System.get_env("DEMIURGE_SECRET_KEY_BASE")
 
+config :cors_plug,
+       origin: ["https://www.evanilukhin.com", "https://evanilukhin.com"],
+       max_age: 86400,
+       methods: ["GET", "POST"],
+       headers: [
+         "Auth-Token",
+         "Content-Type",
+         "Accept",
+         "Origin",
+         "User-Agent",
+         "DNT",
+         "Cache-Control",
+         "X-Mx-ReqToken",
+         "Keep-Alive",
+         "X-Requested-With",
+         "If-Modified-Since",
+         "X-CSRF-Token"
+       ],
+       expose: ["Auth-Token"]
+
 # Configure your database
 config :demiurge, Demiurge.Repo,
   username: System.get_env("PG_USER"),
